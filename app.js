1f81b750db6ca6c0d7947e590605fd894457ec2e
Divyani <divyanikhatri2007@gmail.com>
2026-04-03 11:30:48 +0545

    Initial commit - EMS frontend

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// View engine
+app.set("view engine", "ejs");
+
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index"); // homepage
+});
+
+app.get("/guest", (req, res) => {
+  res.render("guest"); // guest page
+});
+
+app.get("/user", (req, res) => {
+  res.render("user"); // user dashboard
+});
+
+app.get("/event", (req, res)=>{
+  res.render("event")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.get("/eventcreat", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
---
+++ script.js
@@ -0,0 +1,51 @@
+const events = [
+  {
+    name: "Global Synergy: Design & Ethics Summit",
+    date: "2026-10-12",
+    location: "Berlin",
+    category: "Tech",
+  },
+  {
+    name: "Metropolis Night Series: Soundscapes",
+    date: "2026-11-04",
+    location: "Kathmandu",
+    category: "Music",
+  },
+  {
+    name: "The Artisan's Table: Winter Solstice",
+    date: "2026-12-15",
+    location: "Berlin",
+    category: "Food",
+  },
+];
+
+const cards = document.querySelectorAll(".ecard");
+
+function filterEvents() {
+  const search = document.getElementById("searchInput").value.toLowerCase();
+  const date = document.getElementById("dateFilter").value;
+  const location = document.getElementById("locationFilter").value;
+  const category = document.getElementById("categoryFilter").value;
+
+  cards.forEach((card, index) => {
+    const event = events[index];
+
+    const match =
+      event.name.toLowerCase().includes(search) &&
+      (date === "" || event.date === date) &&
+      (location === "" || event.location === location) &&
+      (category === "" || event.category === category);
+
+    card.style.display = match ? "block" : "none";
+  });
+}
+
+document.getElementById("searchInput").addEventListener("input", filterEvents);
+document.getElementById("dateFilter").addEventListener("change", filterEvents);
+document
+  .getElementById("locationFilter")
+  .addEventListener("change", filterEvents);
+document
+  .getElementById("categoryFilter")
+  .addEventListener("change", filterEvents);
+  
