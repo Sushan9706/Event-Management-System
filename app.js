b48fd7b1a2569314f9ee4b0fd2918fa3c13025b9
Shishir Khattri <shishir.studyy@gmail.com>
2026-05-15 09:23:06 +0545

    Minor Bug Fix

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.use(async (req, res, next) => {
 
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
+  res.locals.path = req.path.replace(/\/$/, "") || "/";
   next();
 });
 
